007760ff9ff65cb5567abf04925ee1ff56b4020a
Gkxd <dhua@andrew.cmu.edu>
2015-04-01 22:49:35 -0400

    Readded minimap, moved debug controls to separate script

---
+++ Assets/Scripts/DebugControls.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugControls : MonoBehaviour {
+
+	private Vector3 startPos = new Vector3(41,18,216); //JORDAN'S EDIT DONT CHANGE
+
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftShift)){
+			Vector3 switchPos = startPos;
+			if(startPos.x == 41){
+				startPos=new Vector3(111,152,-1828);
+			}else{
+				startPos=new Vector3(41,18,216);
+			}
+			transform.position=switchPos;
+		}
+	}
+}
