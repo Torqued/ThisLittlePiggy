30e227fb60dd5414d567630f1330fbb72a4860bb
Madeline Duque <mduque@andrew.cmu.edu>
2015-04-03 00:05:26 -0400

    animations added

---
+++ Assets/Scripts/HashIDs.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HashIDs : MonoBehaviour {
+	public int runningBool;
+
+	// Use this for initialization
+	void Awake () {
+		runningBool = Animator.StringToHash("Running");
+	
+	}
+
+}
