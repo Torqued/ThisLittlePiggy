b36a43d06fe0caba989ced90df2f4c9eb6f5084f
Jordan Tick <jrtick@andrew.cmu.edu>
2015-04-03 02:56:21 -0400

    Jordan's level based off map

---
+++ Assets/meshCombine.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class meshCombine : MonoBehaviour {
+
+	public GameObject meshToCombine;
+
+	// Use this for initialization
+	void Start () {
+		OnWizardCreate();
+	}
+	
+	void OnWizardCreate()
+	{
+		if (meshToCombine != null) {
+			MeshFilter[] meshFilters = meshToCombine.GetComponentsInChildren<MeshFilter> ();
+			CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+			int i = 0;
+			while (i < meshFilters.Length) {
+				combine[i].mesh = meshFilters[i].sharedMesh;
+				combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+				//meshFilters[i].gameObject.active = false;
+				i++;
+			}
+			
+			GameObject combinedMesh = meshToCombine;
+			
+
+			combinedMesh.AddComponent<MeshFilter>();
+			combinedMesh.AddComponent<MeshRenderer>();
+			combinedMesh.GetComponent<MeshFilter>().sharedMesh = new Mesh();
+			GameObject.Instantiate(combinedMesh.GetComponent<MeshFilter>().sharedMesh);
+			combinedMesh.GetComponent<MeshFilter>().sharedMesh.CombineMeshes(combine);
+
+			Mesh m1 = combinedMesh.GetComponent<MeshFilter>().mesh;
+			AssetDatabase.CreateAsset(m1, "Assets/pine.asset"); // saves to "assets/"
+			AssetDatabase.SaveAssets();
+			
+			//Object prefab = PrefabUtility.CreateEmptyPrefab( "Assets/" + "CombinedMesh" + ".prefab");
+			//PrefabUtility.ReplacePrefab(combinedMesh, prefab, ReplacePrefabOptions.ConnectToPrefab);
+			
+			
+			
+		}
+	}
+}
