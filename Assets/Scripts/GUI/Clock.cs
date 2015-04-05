5092774ce0cbfe51f193001ebf90429d4acd03b4
Gkxd <dhua@andrew.cmu.edu>
2015-04-04 22:22:28 -0400

    Added clock

---
+++ Assets/Scripts/GUI/Clock.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clock : MonoBehaviour {
+    private DayNightCycle dayNightCycle;
+
+	void Start () {
+        GameObject[] gameControllers = GameObject.FindGameObjectsWithTag("GameController");
+        if (gameControllers.Length != 1) {
+            Debug.LogError("Make sure this scene has exactly 1 object tagged \"GameController\".");
+            Debug.Break();
+        }
+        
+        if ((dayNightCycle = gameControllers[0].GetComponent<DayNightCycle>()) == null) {
+            Debug.LogError("The GameController in this scene must have a \"DayNightCycle\" component.");
+            Debug.Break();
+        }
+	}
+	
+	void Update () {
+        float angle = -77.5f + dayNightCycle.timeOfDay()*360;
+        transform.localEulerAngles = new Vector3(0, 0, angle);
+	}
+}
