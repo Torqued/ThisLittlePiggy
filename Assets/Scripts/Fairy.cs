5300d784b60c1f3fb9c179e829d0a661d669d7a4
bfischer <bf.torqued@gmail.com>
2015-04-09 19:32:15 -0400

    Fairies and test scenes

--- Assets/Scripts/Fairy.cs
+++ Assets/Scripts/Fairy.cs
@@ -4,4 +4,17 @@ using System.Collections;
 public class Fairy : MonoBehaviour {
 
 
+	public GameObject gc;
+	public DayNightCycle dayNight;
+
+	void Start(){
+		gc = GameObject.FindGameObjectWithTag ("GameController");
+		dayNight = gc.GetComponent<DayNightCycle> ();
+	}
+
+	void Update(){
+		if (!dayNight.isNightTime()) {
+			Destroy(gameObject);
+		}
+	}
 }
---
+++ Assets/Scripts/FairyController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FairyController : MonoBehaviour {
+
+	public int numFairies;
+	private bool hasSpawned;
+	private GameObject player;
+	private GameObject gc;
+	private DayNightCycle dayNight;
+	
+	void Start(){
+		player = GameObject.FindGameObjectWithTag("Player");
+		gc = GameObject.FindGameObjectWithTag ("GameController");
+		dayNight = gc.GetComponent<DayNightCycle> ();
+		hasSpawned = false;
+	}
+	
+	void Update(){
+		if (dayNight.isNightTime()) {
+			if(!hasSpawned)
+			{
+				int spanwed = 0;
+				while(spanwed < numFairies)
+				{
+					Object.Instantiate(Resources.Load("SpawnPrefabs/StrawHouse"), player.transform.position, Quaternion.identity);
+				}
+			}
+			hasSpawned = true;
+		}
+		else{
+			hasSpawned = false;
+		}
+	}
+}
