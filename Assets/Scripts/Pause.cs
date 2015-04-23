5041a4c0861ca31ea9c3939b5ae1ee258ddfe4fe
Torqued <bf.torqued@gmail.com>
2015-04-23 12:28:55 -0400

    Pause Script but not in scene

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	bool isPaused;
+
+	// Use this for initialization
+	void Start () {
+		isPaused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.P)) {
+			isPaused = !isPaused;
+		}
+	}
+
+	public bool gameIsPaused(){
+		return isPaused;
+	}
+}
