600f408f50c0edfc78375688c1f4f3e1637b800a
Madeline Duque <mduque@andrew.cmu.edu>
2015-04-23 22:31:42 -0400

    Foliage and GO

---
+++ Assets/Scripts/AlwaysFaceCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlwaysFaceCamera : MonoBehaviour {
+	float initx = 0;
+	float initz = 0;
+
+	void Awake() {
+		initx = transform.rotation.x;
+		initz = transform.rotation.z;
+	}
+	// Update is called once per frame
+	void Update () {
+		Vector3 relativePos = Camera.main.transform.position - transform.position;
+		Quaternion rotation = Quaternion.LookRotation(relativePos);
+		rotation.x = initx;
+		rotation.z = initz;
+		transform.rotation = rotation;
+	}
+}
+
