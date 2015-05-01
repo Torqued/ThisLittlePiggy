feaf0d6c67d83cb7cf754bb21e3ba07cf6bd1c75
Torqued <bf.torqued@gmail.com>
2015-04-30 21:11:08 -0400

    Construction buttons light up on canCraft

--- Assets/Scripts/GUI/Button.cs
+++ Assets/Scripts/GUI/Button.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class Button : MonoBehaviour {
 
+	public enum HouseType {
+		Straw,
+		Sticks,
+		Bricks
+	}
+	
+
     GameObject icon_back;
     GameObject icon_lit;
     GameObject icon_pressed;
@@ -11,6 +18,10 @@ public class Button : MonoBehaviour {
 
     private bool lit;
     private bool pressed;
+	private bool canCraft;
+
+	private CharacterInventory inventory;
+	public HouseType h;
 
 	void Start () {
         icon_back = transform.Find("Icon_Back").gameObject;
@@ -22,6 +33,8 @@ public class Button : MonoBehaviour {
         icon_back.SetActive(true);
         icon_lit.SetActive(false);
         icon_pressed.SetActive(false);
+
+		inventory = GameObject.FindGameObjectWithTag ("Player").GetComponent<CharacterInventory> ();
 	}
 	
     public void setLit() {
@@ -44,6 +57,27 @@ public class Button : MonoBehaviour {
 
             lit = pressed = false;
         }
+		else if((h == HouseType.Sticks && CraftingRecipes.canCraft(Crafting.HouseSticks, inventory))){
+			icon_back.SetActive(false);
+			icon_lit.SetActive(true);
+			icon_pressed.SetActive(pressed);
+			
+			lit = pressed = false;
+		}
+		else if((h == HouseType.Bricks && CraftingRecipes.canCraft(Crafting.HouseBricks, inventory))){
+			icon_back.SetActive(false);
+			icon_lit.SetActive(true);
+			icon_pressed.SetActive(pressed);
+			
+			lit = pressed = false;
+		}
+		else if(((h == HouseType.Straw) && CraftingRecipes.canCraft(Crafting.HouseStraw, inventory))){
+			icon_back.SetActive(false);
+			icon_lit.SetActive(true);
+			icon_pressed.SetActive(pressed);
+			
+			lit = pressed = false;
+		}
         else {
             icon_back.SetActive(true);
             icon_lit.SetActive(false);
